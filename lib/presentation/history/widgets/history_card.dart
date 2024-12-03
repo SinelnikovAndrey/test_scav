7a82da2a479e1018a81efa81d1f8a10674e94e14
Andrey Sinelnikov <sinelnikov.mail@gmail.com>
2024-12-03 14:32:06 +0300

    fix

--- lib/presentation/history/widgets/history_card.dart
+++ lib/presentation/history/widgets/history_card.dart
@@ -52,7 +52,7 @@ class HistoryCard extends StatelessWidget {
                       children: [
                         Center(
                           child: ClipRRect(
-                            borderRadius: BorderRadius.circular(20),
+                            borderRadius: BorderRadius.circular(10),
                             child: SizedBox(
                               height: MediaQuery.of(context).size.height * 0.17,
                               width: MediaQuery.of(context).size.width * 0.4,
